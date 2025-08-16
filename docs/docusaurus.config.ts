ad76e850d36718d3f26db232078c4a4bbabebbaa
Anton Tranelis <mail@antontranelis.de>
2025-08-16 14:33:05 +0200

    fix deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://antontranelis.github.io/it4c-design/',
+  url: 'https://antontranelis.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/it4c-design/',
